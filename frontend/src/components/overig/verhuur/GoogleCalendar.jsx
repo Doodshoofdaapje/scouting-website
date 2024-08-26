31e9582a7f2f7759314f262047ac18ad474b5d98
Doodshoofdaapje <97123208+Doodshoofdaapje@users.noreply.github.com>
2024-08-26 20:33:30 +0200

    Update GoogleCalendar.jsx

--- frontend/src/components/overig/verhuur/GoogleCalendar.jsx
+++ frontend/src/components/overig/verhuur/GoogleCalendar.jsx
@@ -5,7 +5,7 @@ const Calendar = () => {
   return (
     <div className='calendar'>
       <iframe
-        src="https://calendar.google.com/calendar/embed?src=peterselie4231%40gmail.com&ctz=Europe%2FAmsterdam"
+        src="https://calendar.google.com/calendar/u/2/embed?height=700&wkst=2&bgcolor=%23ffffff&ctz=Europe/Amsterdam&src=c2NvdXRpbmdhc2dicmVkYUBnbWFpbC5jb20&src=bmwuZHV0Y2gjaG9saWRheUBncm91cC52LmNhbGVuZGFyLmdvb2dsZS5jb20&color=%239DE05C&color=%230B8043&title=scouting+ASG+Breda&showTz=0&showTabs=1&showDate=1"
         style={{ border: 0 }}
         width="800"
         height="600"
